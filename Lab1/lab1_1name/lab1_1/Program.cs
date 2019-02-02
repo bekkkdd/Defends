b381b2ecd57b2b9eab0cdc147d248af4802a3428
bekkkdd <bekdauletushkempirov@gmail.com>
2019-02-02 21:07:08 +0600

    Lab1

---
+++ Lab1/lab1_1name/lab1_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            // read String from console
+            String s = Console.ReadLine();
+            //parse String to array of chars
+            string[] parts = s.Split();
+
+
+            int count = 0;
+            //checking is the digit is prime, if yes count them
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (isPrime(s[i] - '0')) count++;
+            }
+            // write count to the console
+            Console.WriteLine(count);
+            //write each prime element to console
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (isPrime(s[i] - '0')) Console.Write(s[i] + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+        // func to check
+        static bool isPrime(int a)
+        {
+            if (a < 2) return false;
+
+            for (int i = 2; i * i <= a; i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
