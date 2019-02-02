7ba1f53bf547ec72cbe07c44d62608e3a545e7b2
bekkkdd <bekdauletushkempirov@gmail.com>
2019-02-02 21:11:05 +0600

    Lab1

---
+++ Lab1/task2/task2()/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Student
+    {
+        string name;
+        int id, year;
+
+        public Student(string name, int id)
+        {
+
+        }
+        public String GetName()
+        {
+            return name;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        public void incrementYear(int year)
+        {
+            ++year;
+        }
+    }
+}
