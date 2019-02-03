fcf4060e81e6bc1b6d771cd6d096b2182585f9bf
bekkkdd <bekdauletushkempirov@gmail.com>
2019-02-03 17:41:07 +0600

    lab1

---
+++ Lab1/task3/t3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] arr = new int[a];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            arr = DoubleArray(arr);
+
+            for(int i=0;  i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static int[] DoubleArray(int[] a)
+        {
+            int[] res = new int[a.Length*2];
+            for(int i=0; i < a.Length; i++)
+            {
+                res[i * 2] = a[i];
+                res[i * 2 + 1] = a[i];
+            }
+            return res;
+        }
+    }
+
+
+}
