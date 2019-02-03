5e1470a44ef09e08636b87b29dc4a5ed7fa1cb52
bekkkdd <bekdauletushkempirov@gmail.com>
2019-02-03 18:15:34 +0600

    LAB2

---
+++ Lab2/task1/t1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\Users\Asus\Desktop\Defends\Lab2\task1\input.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string s = "";
+            s = sr.ReadLine();
+            // s = s.Replace(" ","");
+            bool check = true;
+            for(int i=0; i < s.Length; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) Console.Write("YES");
+            else Console.Write("NO");
+
+            fs.Close();
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
