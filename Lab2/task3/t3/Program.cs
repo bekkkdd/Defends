46cb89c7016eb617bb16b56852ccc8c4f616c683
bekkkdd <bekdauletushkempirov@gmail.com>
2019-02-04 18:56:43 +0600

    lab2 task3

---
+++ Lab2/task3/t3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir =new DirectoryInfo(@"C:\Users\Asus\Desktop\Defends");
+            printInfo(dir,0);
+            
+            Console.ReadKey();
+        }
+        static void printInfo(FileSystemInfo dir , int k)
+        {
+            string line = new string('\t', k);
+            line += dir.Name;
+            Console.WriteLine(line);
+            if(dir.GetType() == typeof(DirectoryInfo))
+            {
+                var f = (dir as DirectoryInfo).GetFileSystemInfos();
+                foreach (var i in f)
+                {
+                    printInfo(i, k + 1);
+                }
+            }
+        }
+
+        
+    }
+}
